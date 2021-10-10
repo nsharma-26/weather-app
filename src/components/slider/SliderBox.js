99d9f1abd708c66dce16d24dc063bee1fdfdc083
nsharma-26 <nehasharma261994@gmail.com>
2021-10-10 14:18:09 +0530

    commit weather app changes

--- src/components/slider/SliderBox.js
+++ src/components/slider/SliderBox.js
@@ -97,15 +97,15 @@ class SliderBox extends Component{
             dots: false,
             infinite: false,
             speed: 500,
-            slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToShow: 3,
+            slidesToScroll: 1,
             initialSlide: 0,
             responsive: [
               {
                 breakpoint: 1024,
                 settings: {
                   slidesToShow: 3,
-                  slidesToScroll: 3,
+                  slidesToScroll: 1,
                   infinite: false,
                   dots: false
                 }
@@ -114,7 +114,7 @@ class SliderBox extends Component{
                 breakpoint: 600,
                 settings: {
                   slidesToShow: 2,
-                  slidesToScroll: 2,
+                  slidesToScroll: 1,
                   initialSlide: 2
                 }
               },
